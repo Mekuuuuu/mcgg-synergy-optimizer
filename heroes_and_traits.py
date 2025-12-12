5d1dca166decbec7cc271d24403cf53c0189c3c5
Mekuuuuu <mikoabadia098@gmail.com>
2025-12-13 03:20:24 +0800

    Add heroes and traits game data

---
+++ heroes_and_traits.py
@@ -0,0 +1,82 @@
+traits = [
+    # factions
+    {"name": "Shadowcell", "thresholds": [2, 4, 6, 10]},    # 0 : Shadowcell
+    {"name": "Mortal Rivals", "thresholds": [1, 2]},        # 1 : Mortal Rivals
+    {"name": "KOF", "thresholds": [2, 4, 6, 11]},           # 2 : King of Fighters
+    {"name": "Soul Vessels", "thresholds": [2, 4, 6, 10]},  # 3 : Soul Vessels
+    {"name": "Starwing", "thresholds": [2, 4, 6, 10]},      # 4 : Starwing
+    {"name": "Luminexus", "thresholds": [2, 4, 6, 10]},     # 5 : Luminexus
+    {"name": "Aspirants", "thresholds": [2, 4, 6, 10]},     # 6 : Aspirants
+    {"name": "Toy Mischief", "thresholds": [2, 4, 6, 10]},  # 7 : Toy Mischief
+    {"name": "Glory League", "thresholds": [2, 4, 6]},      # 8 : Glory League
+    {"name": "Metro Zero", "thresholds": [2]},              # 9 : Metro Zero
+    {"name": "Beyond the Clouds", "thresholds": [2, 3]},    # 10 : Beyond the Clouds
+
+    # roles
+    {"name": "Bruiser", "thresholds": [2, 4, 6]},           # 11 : Bruiser
+    {"name": "Defender", "thresholds": [2, 4, 6]},          # 12 : Defender
+    {"name": "Dauntless", "thresholds": [2, 4, 6]},         # 13 : Dauntless
+    {"name": "Weapon Master", "thresholds": [2, 4, 6]},     # 14 : Weapon Master
+    {"name": "Marksman", "thresholds": [2, 4, 6]},          # 15 : Marksman
+    {"name": "Stargazer", "thresholds": [2, 4, 6]},         # 16 : Stargazer
+    {"name": "Swiftblade", "thresholds": [2, 4, 6]},        # 17 : Swiftblade
+    {"name": "Mage", "thresholds": [2, 4, 6]},              # 18 : Mage
+    {"name": "Phasewarper", "thresholds": [2, 4]},          # 19 : Phasewarper
+    {"name": "Scavenger", "thresholds": [2, 3]},            # 20 : Scavenger
+]
+
+heroes = [
+    {"name": "Masha", "star": 3, "traits": [0, 11]},
+    {"name": "Iori Yagami", "star": 5, "traits": [2, 1, 11]},
+    {"name": "Belerick", "star": 3, "traits": [4, 11]},
+    {"name": "Guinivere", "star": 4, "traits": [6, 11]},
+    {"name": "Jawhead", "star": 1, "traits": [7, 11]},
+    {"name": "Aldous", "star": 2, "traits": [8, 11]},
+    {"name": "Terry Bogard", "star": 4, "traits": [2, 12]},
+    {"name": "Atlas", "star": 1, "traits": [4, 12]},
+    {"name": "Ruby", "star": 2, "traits": [6, 13, 12]},
+    {"name": "Uranus", "star": 2, "traits": [7, 12]},
+    {"name": "Minotaur", "star": 3, "traits": [8, 12]},
+    {"name": "Edith", "star": 5, "traits": [10, 12]},
+    {"name": "Chris", "star": 2, "traits": [2, 13]},
+    {"name": "Gloo", "star": 3, "traits": [3, 13]},
+    {"name": "Lolita", "star": 1, "traits": [5, 13]},
+    {"name": "Barats", "star": 4, "traits": [7, 13]},
+    {"name": "X.Borg", "star": 5, "traits": [9, 13]},
+    {"name": "Alucard", "star": 1, "traits": [0, 14]},
+    {"name": "Benedetta", "star": 5, "traits": [3, 14]},
+    {"name": "Freya", "star": 4, "traits": [4, 14]},
+    {"name": "Cici", "star": 2, "traits": [5, 14]},
+    {"name": "Aulus", "star": 3, "traits": [7, 14]},
+    {"name": "Roger", "star": 2, "traits": [9, 14]},
+    {"name": "Obsidia", "star": 2, "traits": [0, 15]},
+    {"name": "Hanabi", "star": 4, "traits": [3, 15]},
+    {"name": "Irithel", "star": 5, "traits": [5, 15]},
+    {"name": "Layla", "star": 1, "traits": [6, 15]},
+    {"name": "Claude", "star": 4, "traits": [8, 15]},
+    {"name": "Ixia", "star": 3, "traits": [9, 15, 16]},
+    {"name": "Kula", "star": 1, "traits": [2, 16]},
+    {"name": "Mathilda", "star": 2, "traits": [4, 16]},
+    {"name": "Kadita", "star": 4, "traits": [5, 16]},
+    {"name": "Vexana", "star": 5, "traits": [6, 16]},
+    {"name": "Xavier", "star": 3, "traits": [10, 16]},
+    {"name": "Helcurt", "star": 1, "traits": [0, 17]},
+    {"name": "K'", "star": 4, "traits": [2, 17]},
+    {"name": "Aamon", "star": 2, "traits": [3, 17]},
+    {"name": "Saber", "star": 4, "traits": [4, 17]},
+    {"name": "Fanny", "star": 3, "traits": [6, 17]},
+    {"name": "Granger", "star": 4, "traits": [8, 17]},
+    {"name": "Valentina", "star": 4, "traits": [0, 18]},
+    {"name": "Kyo Kusanagi", "star": 5, "traits": [2, 1, 18]},
+    {"name": "Cecilion", "star": 1, "traits": [3, 18]},
+    {"name": "Zhuxin", "star": 3, "traits": [5, 18]},
+    {"name": "Harith", "star": 3, "traits": [6, 18]},
+    {"name": "Kagura", "star": 2, "traits": [10, 18]},
+    {"name": "Arlott", "star": 4, "traits": [0, 19]},
+    {"name": "Clint", "star": 2, "traits": [3, 19]},
+    {"name": "Harley", "star": 5, "traits": [4, 19]},
+    {"name": "Lesley", "star": 3, "traits": [6, 19]},
+    {"name": "Leona", "star": 3, "traits": [2, 20]},
+    {"name": "Carmilla", "star": 2, "traits": [5, 20]},
+    {"name": "Akai", "star": 5, "traits": [7, 20]},
+]
